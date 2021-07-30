b841daf9a37aad22af38bc7686c9fcabf5342f93
Adedamola Shittu <shittuadedamola@rocketmail.com>
2021-07-30 10:24:11 +0100

    Add Bugs To Javascript File

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # Portfolio-Bugs-Hide-and-Seek-
+
+## Project Requirements
+
+A bug was intentionally added into this code as required by Microverse in the *Bugs:Hide and Seek* exercise. 
+
+# 👤 Author
+
+GitHub: [@dammyShittu](https://github.com/DammyShittu/)
+
+
+Twitter: [@aded_shittu](https://twitter.com/aded_shittu/)
+
+LinkedIn: [Adedamola Shittu](linkedin.com/in/adedamola-shittu-3ab465172/)
+
+# 🤝 Contributing
+
+Contributions, issues, and feature requests are welcome!
+
+Feel free to check the issues page.
+
+Show your support
+
+Give a ⭐️ if you like this project!
+
+# 📝 License
+
+This project is MIT licensed.
---
+++ js/main.js
@@ -0,0 +1,72 @@
+const mobileMenu = document.getElementById('menu');
+const closeIcon = document.getElementById('close-menu');
+const openIcon = document.getElementById('hamburger-icon');
+
+function menuClose() {
+  mobileMenu.style.left = '-110%';
+}
+
+function menuOpen() {
+  mobileMenu.style.left = '0';
+}
+
+document.querySelectorAll('.classList').forEach((item) => {
+  item.addEventListener('click', () => {
+    mobileMenu.style.left = '-110%';
+  });
+});
+
+closeIcon.addEventListener('click', menuClose);
+openIcon.addEventListener('click', menuOpen);
+
+/* Form Validation */
+const form = document.getElementById('form');
+const email = document.getElementById('email');
+const error = document.querySelector('.error');
+const textarea = document.getElementById('message');
+form.addEventListener('submit', (e) => {
+  if (email.value.toLowerCase() !== email.value) {
+    e.preventDefault();
+    error.style.display = 'block';
+    error.textContent = 'Kindly Use Only Lowercase Letters In Your Email';
+    textarea.style.marginBottom = '10px';
+  }
+});
+
+// Local Storage
+const userName = document.getElementById('name');
+
+// Update User Data
+const getFormData = JSON.parse(localStorage.getItem('formInput'));
+
+if (getFormData) {
+  userName.value = getFormData.username;
+  email.value = getFormData.email;
+  textarea.value = getFormData.message;
+}
+
+// setItem To Local Storage
+function formInfo() {
+  const nameInput = userName.value;
+  const emailInput = email.value;
+  const messageInput = textarea.value;
+
+  const formInput = {
+    username: nameInput,
+    email: emailInput,
+    message: messageInput,
+  };
+
+  localStorage.setItem('formInput', JSON.stringify(formInput));
+}
+
+// Event Listeners
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  formInfo();
+});
+
+userName.onchange = formInfo;
+email.onchange = formInfo;
+textarea.onchange = formInfo;
